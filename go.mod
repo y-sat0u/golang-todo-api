c976fc2be8d23305b6c9565e344b864177150bc0
y-sat0u <yuuuki96g@gmail.com>
2025-12-06 06:18:43 +0900

    config created

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/y-sat0u/golang-todo-api
 go 1.25.5
 
 require golang.org/x/sync v0.18.0
+
+require github.com/caarlos0/env/v11 v11.3.1 // indirect
--- main.go
+++ main.go
@@ -8,10 +8,23 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/y-sat0u/golang-todo-api/config"
 	"golang.org/x/sync/errgroup"
 )
 
-func run(ctx context.Context, l net.Listener) error {
+func run(ctx context.Context) error {
+	cfg, err := config.New()
+	if err != nil {
+		return err
+	}
+	l, err := net.Listen("tcp", fmt.Sprintf(":%d", cfg.Port))
+	if err != nil {
+		log.Fatalf("failed to listen port %d: %v", cfg.Port, err)
+	}
+
+	url := fmt.Sprintf("http://%s", l.Addr().String())
+	log.Printf("start with: %v", url)
+
 	s := &http.Server{
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
@@ -37,16 +50,8 @@ func run(ctx context.Context, l net.Listener) error {
 }
 
 func main() {
-	if len(os.Args) != 2 {
-		log.Printf("need port number\n")
-		os.Exit(1)
-	}
-	p := os.Args[1]
-	l, err := net.Listen("tcp", ":"+p)
-	if err != nil {
-		log.Fatalf("failed to listen port %s: %v", p, err)
-	}
-	if err := run(context.Background(), l); err != nil {
+	if err := run(context.Background()); err != nil {
 		log.Printf("failed to terminate server: %v", err)
+		os.Exit(1)
 	}
 }
